a57ff9b0919500d5a21480aea0724ce290a1d151
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2024-08-03 15:21:31 -0300

    Create replace_keytic.sh

---
+++ keytic/replace_keytic.sh
@@ -0,0 +1,319 @@
+cd /keytic
+
+rm ??-0.wav
+rm ??-1.wav
+
+cp silencio.wav 0a-0.wav
+cp keytic.wav 0a-1.wav
+
+cp silencio.wav 0b-0.wav
+cp keytic.wav 0b-1.wav
+
+cp silencio.wav 0c-0.wav
+cp keytic.wav 0c-1.wav
+
+cp silencio.wav 0d-0.wav
+cp keytic.wav 0d-1.wav
+
+cp silencio.wav 0e-0.wav
+cp keytic.wav 0e-1.wav
+
+cp silencio.wav 0f-0.wav
+cp keytic.wav 0f-1.wav
+
+cp silencio.wav 01-0.wav
+cp keytic.wav 01-1.wav
+
+cp silencio.wav 02-0.wav
+cp keytic.wav 02-1.wav
+
+cp silencio.wav 03-0.wav
+cp keytic.wav 03-1.wav
+
+cp silencio.wav 04-0.wav
+cp keytic.wav 04-1.wav
+
+cp silencio.wav 05-0.wav
+cp keytic.wav 05-1.wav
+
+cp silencio.wav 06-0.wav
+cp keytic.wav 06-1.wav
+
+cp silencio.wav 07-0.wav
+cp keytic.wav 07-1.wav
+
+cp silencio.wav 08-0.wav
+cp keytic.wav 08-1.wav
+
+cp silencio.wav 09-0.wav
+cp keytic.wav 09-1.wav
+
+cp silencio.wav 1a-0.wav
+cp keytic.wav 1a-1.wav
+
+cp silencio.wav 1b-0.wav
+cp keytic.wav 1b-1.wav
+
+cp silencio.wav 1c-0.wav
+cp keytic.wav 1c-1.wav
+
+cp silencio.wav 1d-0.wav
+cp keytic.wav 1d-1.wav
+
+cp silencio.wav 1e-0.wav
+cp keytic.wav 1e-1.wav
+
+cp silencio.wav 1f-0.wav
+cp keytic.wav 1f-1.wav
+
+cp silencio.wav 2a-0.wav
+cp keytic.wav 2a-1.wav
+
+cp silencio.wav 2b-0.wav
+cp keytic.wav 2b-1.wav
+
+cp silencio.wav 2c-0.wav
+cp keytic.wav 2c-1.wav
+
+cp silencio.wav 2d-0.wav
+cp keytic.wav 2d-1.wav
+
+cp silencio.wav 2e-0.wav
+cp keytic.wav 2e-1.wav
+
+cp silencio.wav 2f-0.wav
+cp keytic.wav 2f-1.wav
+
+cp silencio.wav 3a-0.wav
+cp keytic.wav 3a-1.wav
+
+cp silencio.wav 3b-0.wav
+cp keytic.wav 3b-1.wav
+
+cp silencio.wav 3c-0.wav
+cp keytic.wav 3c-1.wav
+
+cp silencio.wav 3d-0.wav
+cp keytic.wav 3d-1.wav
+
+cp silencio.wav 3e-0.wav
+cp keytic.wav 3e-1.wav
+
+cp silencio.wav 3f-0.wav
+cp keytic.wav 3f-1.wav
+
+cp silencio.wav 4a-0.wav
+cp keytic.wav 4a-1.wav
+
+cp silencio.wav 4b-0.wav
+cp keytic.wav 4b-1.wav
+
+cp silencio.wav 4c-0.wav
+cp keytic.wav 4c-1.wav
+
+cp silencio.wav 4d-0.wav
+cp keytic.wav 4d-1.wav
+
+cp silencio.wav 4e-0.wav
+cp keytic.wav 4e-1.wav
+
+cp silencio.wav 4f-0.wav
+cp keytic.wav 4f-1.wav
+
+cp silencio.wav 5b-0.wav
+cp keytic.wav 5b-1.wav
+
+cp silencio.wav 6a-0.wav
+cp keytic.wav 6a-1.wav
+
+cp silencio.wav 6b-0.wav
+cp keytic.wav 6b-1.wav
+
+cp silencio.wav 6c-0.wav
+cp keytic.wav 6c-1.wav
+
+cp silencio.wav 6d-0.wav
+cp keytic.wav 6d-1.wav
+
+cp silencio.wav 6e-0.wav
+cp keytic.wav 6e-1.wav
+
+cp silencio.wav 6f-0.wav
+cp keytic.wav 6f-1.wav
+
+cp silencio.wav 7d-0.wav
+cp keytic.wav 7d-1.wav
+
+cp silencio.wav 10-0.wav
+cp keytic.wav 10-1.wav
+
+cp silencio.wav 11-0.wav
+cp keytic.wav 11-1.wav
+
+cp silencio.wav 12-0.wav
+cp keytic.wav 12-1.wav
+
+cp silencio.wav 13-0.wav
+cp keytic.wav 13-1.wav
+
+cp silencio.wav 14-0.wav
+cp keytic.wav 14-1.wav
+
+cp silencio.wav 15-0.wav
+cp keytic.wav 15-1.wav
+
+cp silencio.wav 16-0.wav
+cp keytic.wav 16-1.wav
+
+cp silencio.wav 17-0.wav
+cp keytic.wav 17-1.wav
+
+cp silencio.wav 18-0.wav
+cp keytic.wav 18-1.wav
+
+cp silencio.wav 19-0.wav
+cp keytic.wav 19-1.wav
+
+cp silencio.wav 20-0.wav
+cp keytic.wav 20-1.wav
+
+cp silencio.wav 21-0.wav
+cp keytic.wav 21-1.wav
+
+cp silencio.wav 22-0.wav
+cp keytic.wav 22-1.wav
+
+cp silencio.wav 23-0.wav
+cp keytic.wav 23-1.wav
+
+cp silencio.wav 24-0.wav
+cp keytic.wav 24-1.wav
+
+cp silencio.wav 25-0.wav
+cp keytic.wav 25-1.wav
+
+cp silencio.wav 26-0.wav
+cp keytic.wav 26-1.wav
+
+cp silencio.wav 27-0.wav
+cp keytic.wav 27-1.wav
+
+cp silencio.wav 28-0.wav
+cp keytic.wav 28-1.wav
+
+cp silencio.wav 29-0.wav
+cp keytic.wav 29-1.wav
+
+cp silencio.wav 30-0.wav
+cp keytic.wav 30-1.wav
+
+cp silencio.wav 31-0.wav
+cp keytic.wav 31-1.wav
+
+cp silencio.wav 32-0.wav
+cp keytic.wav 32-1.wav
+
+cp silencio.wav 33-0.wav
+cp keytic.wav 33-1.wav
+
+cp silencio.wav 34-0.wav
+cp keytic.wav 34-1.wav
+
+cp silencio.wav 35-0.wav
+cp keytic.wav 35-1.wav
+
+cp silencio.wav 36-0.wav
+cp keytic.wav 36-1.wav
+
+cp silencio.wav 37-0.wav
+cp keytic.wav 37-1.wav
+
+cp silencio.wav 38-0.wav
+cp keytic.wav 38-1.wav
+
+cp silencio.wav 39-0.wav
+cp keytic.wav 39-1.wav
+
+cp silencio.wav 40-0.wav
+cp keytic.wav 40-1.wav
+
+cp silencio.wav 41-0.wav
+cp keytic.wav 41-1.wav
+
+cp silencio.wav 42-0.wav
+cp keytic.wav 42-1.wav
+
+cp silencio.wav 43-0.wav
+cp keytic.wav 43-1.wav
+
+cp silencio.wav 44-0.wav
+cp keytic.wav 44-1.wav
+
+cp silencio.wav 45-0.wav
+cp keytic.wav 45-1.wav
+
+cp silencio.wav 46-0.wav
+cp keytic.wav 46-1.wav
+
+cp silencio.wav 47-0.wav
+cp keytic.wav 47-1.wav
+
+cp silencio.wav 48-0.wav
+cp keytic.wav 48-1.wav
+
+cp silencio.wav 49-0.wav
+cp keytic.wav 49-1.wav
+
+cp silencio.wav 50-0.wav
+cp keytic.wav 50-1.wav
+
+cp silencio.wav 51-0.wav
+cp keytic.wav 51-1.wav
+
+cp silencio.wav 52-0.wav
+cp keytic.wav 52-1.wav
+
+cp silencio.wav 53-0.wav
+cp keytic.wav 53-1.wav
+
+cp silencio.wav 56-0.wav
+cp keytic.wav 56-1.wav
+
+cp silencio.wav 57-0.wav
+cp keytic.wav 57-1.wav
+
+cp silencio.wav 58-0.wav
+cp keytic.wav 58-1.wav
+
+cp silencio.wav 60-0.wav
+cp keytic.wav 60-1.wav
+
+cp silencio.wav 61-0.wav
+cp keytic.wav 61-1.wav
+
+cp silencio.wav 62-0.wav
+cp keytic.wav 62-1.wav
+
+cp silencio.wav 63-0.wav
+cp keytic.wav 63-1.wav
+
+cp silencio.wav 64-0.wav
+cp keytic.wav 64-1.wav
+
+cp silencio.wav 66-0.wav
+cp keytic.wav 66-1.wav
+
+cp silencio.wav 67-0.wav
+cp keytic.wav 67-1.wav
+
+cp silencio.wav 68-0.wav
+cp keytic.wav 68-1.wav
+
+cp silencio.wav 69-0.wav
+cp keytic.wav 69-1.wav
+
+cp silencio.wav 77-0.wav
+cp keytic.wav 77-1.wav
+
+#cp silencio.wav ff-0.wav # soltar clic de mouse
+#cp keytic.wav ff-1.wav   # clic del mouse 
